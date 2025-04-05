04bc6994386dd0f6fbe5258211551a2b138d672b
Guilherme Garcia <garciaguig@gmail.com>
2025-04-05 18:11:55 -0300

    build: Config ESLint

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+  extends: [
+    'next',
+    'next/core-web-vitals',
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+  ],
+  plugins: ['react', '@typescript-eslint'],
+  parserOptions: {
+    ecmaVersion: 2021,
+    sourceType: 'module',
+  },
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+    'react/jsx-filename-extension': [1, { extensions: ['.ts', '.tsx'] }],
+  },
+  overrides: [
+    {
+      "files": ["src/components/ui/**"],
+      "rules": {
+        "react/prop-types": "off"
+      }
+    }
+  ]
+}
