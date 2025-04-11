4849b7df9831a7b8e5c6814a353f960a4a55dcb5
Guilherme Garcia <garciaguig@gmail.com>
2025-04-11 11:11:50 -0300

    feat: Add home page redirection logic in next js middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,19 +1,30 @@
-import { withAuth } from 'next-auth/middleware'
+import { NextRequestWithAuth, withAuth } from 'next-auth/middleware'
+import { NextResponse } from 'next/server'
 
-export default withAuth(function middleware() {}, {
-  pages: {
-    signIn: '/auth/login',
+export default withAuth(
+  function middleware(req: NextRequestWithAuth) {
+    const BASE_URL = req.url
+    const CURRENT_URL = req.nextUrl.pathname
+
+    if (CURRENT_URL === '/') {
+      return NextResponse.redirect(new URL('/home', BASE_URL))
+    }
   },
+  {
+    pages: {
+      signIn: '/auth/login',
+    },
 
-  callbacks: {
-    async authorized(data) {
-      const token = data.token
+    callbacks: {
+      async authorized(data) {
+        const token = data.token
 
-      if (token) return true
-      else return false
+        if (token) return true
+        else return false
+      },
     },
   },
-})
+)
 
 export const config = {
   matcher: [
