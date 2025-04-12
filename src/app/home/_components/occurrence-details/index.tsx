4c3221000c7fa6267a1dd86d42535888aa9762b1
Guilherme Garcia <garciaguig@gmail.com>
2025-04-12 16:40:25 -0300

    feat: Add image and description elements to 'occurrence details' component

--- src/app/home/_components/occurrence-details/index.tsx
+++ src/app/home/_components/occurrence-details/index.tsx
@@ -5,7 +5,9 @@ import {
   DialogHeader,
   DialogTitle,
 } from '@/components/ui/dialog'
+import { Separator } from '@/components/ui/separator'
 import { Occurrence } from '@prisma/client'
+import Image from 'next/image'
 import { MetaInfo } from './_components'
 
 interface Props {
@@ -38,6 +40,22 @@ export default function OccurrenceDetails({ data, isOpen, handleOpen }: Props) {
 
         <div className="space-y-4">
           <MetaInfo createdAt={createdAt} id={id} status={status} />
+          <Separator />
+
+          <div className="overflow-hidden rounded-md">
+            <Image
+              src={image}
+              alt="Imagem da ocorrência"
+              width={800}
+              height={400}
+              className="h-auto max-h-[400px] w-full rounded-lg object-cover"
+            />
+          </div>
+
+          <div>
+            <h3 className="text-base font-medium">Descrição</h3>
+            <p className="text-sm text-muted-foreground">{description}</p>
+          </div>
         </div>
       </DialogContent>
     </Dialog>
