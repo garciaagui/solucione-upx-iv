22d89f5fa879d6fa9d498c5d9d31dea78670dd39
Guilherme Garcia <garciaguig@gmail.com>
2025-04-12 15:25:32 -0300

    feat: Implement 'occurrence details' component

--- src/app/home/_components/occurrences-table.tsx
+++ src/app/home/_components/occurrences-table.tsx
@@ -8,6 +8,8 @@ import {
   TableRow,
 } from '@/components/ui/table'
 import { Occurrence } from '@prisma/client'
+import { useState } from 'react'
+import OccurrenceDetails from './occurrence-details'
 
 interface Props {
   data: Occurrence[] | undefined
@@ -15,7 +17,6 @@ interface Props {
 
 const formatDate = (input: Date): string => {
   const date = new Date(input)
-
   const day = String(date.getDate()).padStart(2, '0')
   const month = String(date.getMonth() + 1).padStart(2, '0')
   const year = date.getFullYear()
@@ -24,39 +25,65 @@ const formatDate = (input: Date): string => {
 }
 
 export default function OccurrencesTable({ data }: Props) {
+  const [openDetails, setOpenDetails] = useState(false)
+  const [selectedOccurrence, setSelectedOccurrence] = useState<Occurrence | null>(null)
+
+  const handleOpenDetails = (occurrence?: Occurrence) => {
+    if (occurrence) {
+      setSelectedOccurrence(occurrence)
+      setOpenDetails(true)
+    } else {
+      setOpenDetails(false)
+    }
+  }
+
   return (
-    <Table>
-      <TableHeader>
-        <TableRow>
-          <TableHead>Identificação</TableHead>
-          <TableHead>Título</TableHead>
-          <TableHead>Data</TableHead>
-          <TableHead>Status</TableHead>
-          <TableHead>Bairro</TableHead>
-          <TableHead>Logradouro</TableHead>
-        </TableRow>
-      </TableHeader>
+    <>
+      <Table>
+        <TableHeader>
+          <TableRow>
+            <TableHead>Identificação</TableHead>
+            <TableHead>Título</TableHead>
+            <TableHead>Data</TableHead>
+            <TableHead>Status</TableHead>
+            <TableHead>Bairro</TableHead>
+            <TableHead>Logradouro</TableHead>
+          </TableRow>
+        </TableHeader>
 
-      <TableBody>
-        {data &&
-          data.map((occurrence, index) => {
-            const { createdAt, id, neighborhood, status, street, title } = occurrence
-            const formattedDate = formatDate(createdAt)
+        <TableBody>
+          {data &&
+            data.map((occurrence, index) => {
+              const { createdAt, id, neighborhood, status, street, title } = occurrence
+              const formattedDate = formatDate(createdAt)
 
-            return (
-              <TableRow key={index}>
-                <TableCell>{id}</TableCell>
-                <TableCell>{title}</TableCell>
-                <TableCell>{formattedDate}</TableCell>
-                <TableCell>
-                  <Badge>{status}</Badge>
-                </TableCell>
-                <TableCell>{neighborhood}</TableCell>
-                <TableCell>{street}</TableCell>
-              </TableRow>
-            )
-          })}
-      </TableBody>
-    </Table>
+              return (
+                <TableRow
+                  key={index}
+                  onClick={() => handleOpenDetails(occurrence)}
+                  className="cursor-pointer hover:bg-muted"
+                >
+                  <TableCell>{id}</TableCell>
+                  <TableCell>{title}</TableCell>
+                  <TableCell>{formattedDate}</TableCell>
+                  <TableCell>
+                    <Badge>{status}</Badge>
+                  </TableCell>
+                  <TableCell>{neighborhood}</TableCell>
+                  <TableCell>{street}</TableCell>
+                </TableRow>
+              )
+            })}
+        </TableBody>
+      </Table>
+
+      {selectedOccurrence && (
+        <OccurrenceDetails
+          data={selectedOccurrence}
+          isOpen={openDetails}
+          handleOpen={handleOpenDetails}
+        />
+      )}
+    </>
   )
 }
