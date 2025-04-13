79933496d9f6078a346ba5aba2b7f126f738ab09
Guilherme Garcia <garciaguig@gmail.com>
2025-04-13 17:11:24 -0300

    feat: Implement initial structure for create occurrence api route

--- src/app/api/occurrences/route.tsx
+++ src/app/api/occurrences/route.tsx
@@ -1,6 +1,8 @@
+import awsS3 from '@/lib/awsS3'
 import prisma from '@/lib/prisma'
 import { HttpException, NotFoundException } from '@/utils/exceptions'
-import { NextResponse } from 'next/server'
+import { PutObjectRequest } from 'aws-sdk/clients/s3'
+import { NextRequest, NextResponse } from 'next/server'
 
 export const dynamic = 'force-dynamic'
 
@@ -33,5 +35,42 @@ export async function GET(): Promise<NextResponse> {
 
       return NextResponse.json({ message }, { status: 500 })
     }
+  }
+}
+
+const uploadImage = async (file: File) => {
+  const arrayBuffer = await file.arrayBuffer()
+  const buffer = Buffer.from(arrayBuffer)
+
+  const params: PutObjectRequest = {
+    Bucket: 'upx',
+    Key: 'TESTE1',
+    Body: arrayBuffer,
+    ContentType: file.type,
+  }
+
+  await awsS3.upload(params).promise()
+}
+
+export async function POST(req: NextRequest): Promise<NextResponse> {
+  try {
+    const body = await req.formData()
+    const image = body.get('image') as File
+
+    await uploadImage(image)
+
+    return NextResponse.json(
+      {
+        message: 'Ocorrência criada!',
+      },
+      { status: 201 },
+    )
+  } catch (error: unknown) {
+    const message = error instanceof HttpException ? error.message : 'Erro inesperado'
+    const status = error instanceof HttpException ? error.status : 500
+
+    console.error(message, error)
+
+    return NextResponse.json({ message }, { status })
   }
 }
