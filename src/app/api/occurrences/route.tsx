1951c4a5ebcb4bc1d7297b3bb42326d3b8021c6f
Guilherme Garcia <garciaguig@gmail.com>
2025-04-13 19:33:28 -0300

    feat: Finish create occurrence api route logic

--- src/app/api/occurrences/route.tsx
+++ src/app/api/occurrences/route.tsx
@@ -1,7 +1,9 @@
 import awsS3 from '@/lib/awsS3'
 import prisma from '@/lib/prisma'
 import { HttpException, NotFoundException } from '@/utils/exceptions'
+import { Status } from '@prisma/client'
 import { PutObjectRequest } from 'aws-sdk/clients/s3'
+import { randomBytes } from 'crypto'
 import { NextRequest, NextResponse } from 'next/server'
 
 export const dynamic = 'force-dynamic'
@@ -38,30 +40,87 @@ export async function GET(): Promise<NextResponse> {
   }
 }
 
-const uploadImage = async (file: File) => {
-  const arrayBuffer = await file.arrayBuffer()
-  const buffer = Buffer.from(arrayBuffer)
-
+const uploadImage = async (file: Buffer, name: string) => {
   const params: PutObjectRequest = {
     Bucket: 'upx',
-    Key: 'TESTE1',
-    Body: buffer,
-    ContentType: file.type,
+    Key: name,
+    Body: file,
+    ContentType: 'image/jpeg',
   }
 
   await awsS3.upload(params).promise()
 }
 
+const convertFileToBuffer = async (file: File) => {
+  const arrayBuffer = await file.arrayBuffer()
+  const buffer = Buffer.from(arrayBuffer)
+
+  return buffer
+}
+
+const getFileExtension = (originalName: string) => {
+  const parts = originalName.split('.')
+  return parts[parts.length - 1]
+}
+
+const generateUniqueFileName = (originalName: string) => {
+  const fileExtension = getFileExtension(originalName)
+
+  const bytes = randomBytes(16)
+  const fileName = bytes.toString('hex') + Date.now() + '.' + fileExtension
+  return fileName
+}
+
+const parseFormData = async (req: NextRequest) => {
+  const formData = await req.formData()
+  const entries = Array.from(formData.entries())
+
+  const fields: Record<string, string> = {}
+
+  entries.forEach(([key, value]) => {
+    if (value instanceof File) return
+
+    fields[key] = value.toString()
+  })
+
+  const file = formData.get('image') as File
+  const buffer = await convertFileToBuffer(file)
+
+  return {
+    fields,
+    image: { buffer, name: file.name },
+  }
+}
+
 export async function POST(req: NextRequest): Promise<NextResponse> {
   try {
-    const body = await req.formData()
-    const image = body.get('image') as File
+    const { fields, image } = await parseFormData(req)
+    const { buffer, name } = image
+    const imageUniqueName = generateUniqueFileName(name)
+    const imageUrl = process.env.NEXT_PUBLIC_CLOUDFLARE_R2_PUBLIC_ENDPOINT + '/' + imageUniqueName
 
-    await uploadImage(image)
+    await uploadImage(buffer, imageUniqueName)
+
+    const creationData = {
+      title: fields.title,
+      description: fields.description,
+      street: fields.street,
+      neighborhood: fields.neighborhood,
+      zipCode: fields.zipCode,
+      reference: fields.reference || null,
+      image: imageUrl,
+      status: Status.Aberto,
+      userId: Number(fields.userId),
+    }
+
+    const created = await prisma.occurrence.create({
+      data: creationData,
+    })
 
     return NextResponse.json(
       {
         message: 'Ocorrência criada!',
+        data: created,
       },
       { status: 201 },
     )
