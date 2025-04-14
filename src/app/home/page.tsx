70da9a0b2b8830161d42c1ddd81ae7bb3bcf82e7
Guilherme Garcia <garciaguig@gmail.com>
2025-04-14 17:33:26 -0300

    feat: Create and implement 'dialog button' component in home page

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,31 +1,15 @@
 'use client'
 
-import LoginDialog from '@/components/login-dialog'
-import { Button } from '@/components/ui/button'
-import { useSession } from 'next-auth/react'
-import { useState } from 'react'
-import { CreateOccurrence, OccurrencesTable } from './_components'
+import { DialogButton, OccurrencesTable } from './_components'
 import { useOccurrences } from './_utils/useOccurrences'
 
 export default function Home() {
-  const [openDialog, setOpenDialog] = useState(false)
   const { data } = useOccurrences()
-  const { status } = useSession()
-
-  const isLoggedIn = status === 'authenticated'
 
   return (
     <div>
       <h1>Início</h1>
-
-      <Button onClick={() => setOpenDialog(!openDialog)}>Abrir reclamação</Button>
-
-      {isLoggedIn ? (
-        <CreateOccurrence isOpen={openDialog} handleOpen={setOpenDialog} />
-      ) : (
-        <LoginDialog isOpen={openDialog} handleOpen={setOpenDialog} />
-      )}
-
+      <DialogButton />
       <OccurrencesTable data={data} />
     </div>
   )
