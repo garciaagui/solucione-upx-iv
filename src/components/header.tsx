89dbd6ba8dfb5382a557a43050402d0973a697f5
Guilherme Garcia <garciaguig@gmail.com>
2025-04-14 20:31:26 -0300

    feat: Create 'header' component

---
+++ src/components/header.tsx
@@ -0,0 +1,14 @@
+import { SidebarTrigger } from '@/components/ui/sidebar'
+
+export default function Header() {
+  return (
+    <div className="sticky top-0 flex shrink-0 items-center gap-2 border-b p-4">
+      <SidebarTrigger />
+      <div className="relative flex flex-row items-center gap-2">
+        <span className="text-2xl font-semibold tracking-tight text-sidebar-primary">
+          Solucione
+        </span>
+      </div>
+    </div>
+  )
+}
