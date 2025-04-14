47caa2121214f65823e9be21e8152dc9502d9e7d
Guilherme Garcia <garciaguig@gmail.com>
2025-04-14 16:28:06 -0300

    feat: Implement 'login dialog' in home page

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,15 +1,31 @@
 'use client'
 
+import LoginDialog from '@/components/login-dialog'
+import { Button } from '@/components/ui/button'
+import { useSession } from 'next-auth/react'
+import { useState } from 'react'
 import { CreateOccurrence, OccurrencesTable } from './_components'
 import { useOccurrences } from './_utils/useOccurrences'
 
 export default function Home() {
+  const [openDialog, setOpenDialog] = useState(false)
   const { data } = useOccurrences()
+  const { status } = useSession()
+
+  const isLoggedIn = status === 'authenticated'
 
   return (
     <div>
       <h1>Início</h1>
-      <CreateOccurrence />
+
+      <Button onClick={() => setOpenDialog(!openDialog)}>Abrir reclamação</Button>
+
+      {isLoggedIn ? (
+        <CreateOccurrence isOpen={openDialog} handleOpen={setOpenDialog} />
+      ) : (
+        <LoginDialog isOpen={openDialog} handleOpen={setOpenDialog} />
+      )}
+
       <OccurrencesTable data={data} />
     </div>
   )
