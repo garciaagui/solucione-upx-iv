77a9c5667b3e61df447acff18ab09e6b3852471a
Guilherme Garcia <garciaguig@gmail.com>
2025-04-15 12:57:41 -0300

    refactor: Clean up home page 'actions grid' component

--- src/app/home/_components/actions-grid/index.tsx
+++ src/app/home/_components/actions-grid/index.tsx
@@ -1,30 +1,3 @@
-import LoginDialog from '@/components/login-dialog'
-import { Button } from '@/components/ui/button'
-import { SquareArrowOutUpRight } from 'lucide-react'
-import { useSession } from 'next-auth/react'
-import { useState } from 'react'
-import { CreateOccurrence } from './_components'
-
 export default function ActionsGrid() {
-  const [openDialog, setOpenDialog] = useState(false)
-  const { status } = useSession()
-
-  const isLoggedIn = status === 'authenticated'
-
-  return (
-    <>
-      <div>
-        <Button variant="outline" onClick={() => setOpenDialog(!openDialog)}>
-          <SquareArrowOutUpRight />
-          <span>Abrir reclamação</span>
-        </Button>
-      </div>
-
-      {isLoggedIn ? (
-        <CreateOccurrence isOpen={openDialog} handleOpen={setOpenDialog} />
-      ) : (
-        <LoginDialog isOpen={openDialog} handleOpen={setOpenDialog} />
-      )}
-    </>
-  )
+  return <div></div>
 }
