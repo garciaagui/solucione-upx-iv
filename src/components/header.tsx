01696c40255a743e9a46a44409d121b5db520a22
Guilherme Garcia <garciaguig@gmail.com>
2025-04-15 09:56:51 -0300

    fix: Fix styles bug 'header' component

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,7 +2,7 @@ import { SidebarTrigger } from '@/components/ui/sidebar'
 
 export default function Header() {
   return (
-    <div className="sticky top-0 flex shrink-0 items-center gap-2 border-b p-4">
+    <div className="sticky top-0 z-10 flex shrink-0 items-center gap-2 border-b bg-background p-4">
       <SidebarTrigger />
       <div className="relative flex flex-row items-center gap-2">
         <span className="text-2xl font-semibold tracking-tight text-sidebar-primary">
