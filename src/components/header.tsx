2e83eb238a960210dee428787014941569952e5b
Guilherme Garcia <garciaguig@gmail.com>
2025-04-15 12:54:01 -0300

    feat: Add create occurrence button to 'header' component

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,12 +1,48 @@
+'use client'
+
+import { CreateOccurrence } from '@/app/home/_components/actions-grid/_components'
 import { SidebarTrigger } from '@/components/ui/sidebar'
+import { BookPlus } from 'lucide-react'
+import { useSession } from 'next-auth/react'
+import { useState } from 'react'
+import LoginDialog from './login-dialog'
+import { Button } from './ui/button'
+import { Separator } from './ui/separator'
+
+function CreateOccurrenceButton() {
+  const [openDialog, setOpenDialog] = useState(false)
+  const { status } = useSession()
+
+  const isLoggedIn = status === 'authenticated'
+
+  return (
+    <>
+      <Button variant="outline" size="sm" onClick={() => setOpenDialog(!openDialog)}>
+        <BookPlus />
+        <span>Abrir reclamação</span>
+      </Button>
+
+      {isLoggedIn ? (
+        <CreateOccurrence isOpen={openDialog} handleOpen={setOpenDialog} />
+      ) : (
+        <LoginDialog isOpen={openDialog} handleOpen={setOpenDialog} />
+      )}
+    </>
+  )
+}
 
 export default function Header() {
   return (
-    <div className="sticky top-0 z-10 flex shrink-0 items-center gap-2 border-b bg-background p-4">
-      <SidebarTrigger />
-      <div className="relative flex flex-row items-center gap-2">
+    <div className="sticky top-0 z-10 flex shrink-0 items-center gap-8 border-b bg-background p-4">
+      <div className="flex items-center gap-2">
+        <SidebarTrigger />
+
         <span className="text-2xl font-semibold tracking-tight text-primary">Solucione</span>
       </div>
+
+      <Separator orientation="vertical" />
+
+      <CreateOccurrenceButton />
     </div>
   )
 }
