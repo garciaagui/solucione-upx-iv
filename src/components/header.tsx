26d065340e0ce81646dab0f399d05dd7885ea93e
Guilherme Garcia <garciaguig@gmail.com>
2025-04-15 11:47:42 -0300

    fix: Fix header title text color

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -5,9 +5,7 @@ export default function Header() {
     <div className="sticky top-0 z-10 flex shrink-0 items-center gap-2 border-b bg-background p-4">
       <SidebarTrigger />
       <div className="relative flex flex-row items-center gap-2">
-        <span className="text-2xl font-semibold tracking-tight text-sidebar-primary">
-          Solucione
-        </span>
+        <span className="text-2xl font-semibold tracking-tight text-primary">Solucione</span>
       </div>
     </div>
   )
