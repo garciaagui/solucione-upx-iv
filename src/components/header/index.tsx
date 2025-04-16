deb438946ab69052bb82b6215150a52933fe24c7
Guilherme Garcia <garciaguig@gmail.com>
2025-04-16 18:11:47 -0300

    fix: Fix z-index from 'header' component

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -5,7 +5,7 @@ import { CreateOccurrenceButton, SidebarButton } from './_components'
 
 export default function Header() {
   return (
-    <div className="sticky top-0 z-10 flex shrink-0 items-center gap-8 border-b bg-background p-4">
+    <div className="sticky top-0 z-[999] flex shrink-0 items-center gap-8 border-b bg-background p-4">
       <div className="flex items-center gap-2">
         <SidebarButton />
         <span className="text-2xl font-semibold tracking-tight text-primary">Solucione</span>
