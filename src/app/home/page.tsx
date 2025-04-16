06544b11b37123bd73c446e45f130ae18ddfbb7d
Guilherme Garcia <garciaguig@gmail.com>
2025-04-16 18:27:59 -0300

    feat: Implement loading skeleton logic in home page

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,14 +1,21 @@
 'use client'
 
 import { useOccurrences } from '@/hooks/use-occurrences'
+import { useSession } from 'next-auth/react'
 import { OccurrenceCard } from './_components'
+import { SkeletonCard } from './_components/skeleton-card'
 
 export default function Home() {
-  const { data } = useOccurrences()
+  const { data, isLoading: isLoadingData } = useOccurrences()
+  const { status } = useSession()
+
+  const isLoading = isLoadingData || status === 'loading'
 
   return (
-    <div className="flex flex-col gap-6">
-      {data?.map((occurrence) => <OccurrenceCard key={occurrence.id} data={occurrence} />)}
+    <div className="flex flex-col gap-12">
+      {isLoading || !data
+        ? Array.from({ length: 3 }).map((_, i) => <SkeletonCard key={i} />)
+        : data.map((occurrence) => <OccurrenceCard key={occurrence.id} data={occurrence} />)}
     </div>
   )
 }
