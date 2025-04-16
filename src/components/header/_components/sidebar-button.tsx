e1147e0be5611ba8f26687795b6fd75d4db1248b
Guilherme Garcia <garciaguig@gmail.com>
2025-04-16 13:07:44 -0300

    feat: Add loading logic for header 'sidebar button' component

--- src/components/header/_components/sidebar-button.tsx
+++ src/components/header/_components/sidebar-button.tsx
@@ -1,8 +1,15 @@
 import { SidebarTrigger, useSidebar } from '@/components/ui/sidebar'
+import { Skeleton } from '@/components/ui/skeleton'
 import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from '@/components/ui/tooltip'
+import { useSession } from 'next-auth/react'
 
 export default function SidebarButton() {
   const { open } = useSidebar()
+  const { status } = useSession()
+
+  const isLoading = status === 'loading'
+
+  if (isLoading) return <Skeleton className="size-7 rounded-lg" />
 
   return (
     <TooltipProvider delayDuration={100}>
