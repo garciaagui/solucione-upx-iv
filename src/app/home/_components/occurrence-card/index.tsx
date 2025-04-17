b758dc1a1fffb88d95367bcf37352ce404b17cf2
Guilherme Garcia <garciaguig@gmail.com>
2025-04-16 21:53:09 -0300

    feat: Add occurrence page navigation logic to 'occurrence card' footer

--- src/app/home/_components/occurrence-card/index.tsx
+++ src/app/home/_components/occurrence-card/index.tsx
@@ -3,6 +3,7 @@ import { Card, CardContent, CardFooter, CardHeader } from '@/components/ui/card'
 import { OccurrenceWithRelations } from '@/types/globals'
 import { formatDate } from '@/utils/functions/date'
 import { ArrowRight, CalendarDays, MapPin } from 'lucide-react'
+import Link from 'next/link'
 import { ImageContainer } from './_components'
 
 interface Props {
@@ -10,12 +11,11 @@ interface Props {
 }
 
 export default function OccurrenceCard({ data }: Props) {
-  const { title, createdAt, status, neighborhood, street, image, user } = data
+  const { createdAt, id, image, neighborhood, status, title, street, user } = data
 
   return (
     <Card className="relative mx-auto w-full max-w-2xl overflow-hidden rounded-2xl border border-muted bg-background shadow-md transition-shadow hover:shadow-xl">
       <ImageContainer title={title} url={image} username={user.name} />
-
       <CardHeader className="flex flex-row items-center justify-between px-4 text-muted-foreground">
         <StatusBadge status={status} />
         <div className="flex items-center gap-1 text-xs">
@@ -23,7 +23,6 @@ export default function OccurrenceCard({ data }: Props) {
           <span>{formatDate(createdAt)}</span>
         </div>
       </CardHeader>
-
       <CardContent className="flex items-center gap-2 px-4 pb-4 text-sm text-muted-foreground">
         <MapPin size={16} />
         <span>
@@ -31,10 +30,12 @@ export default function OccurrenceCard({ data }: Props) {
         </span>
       </CardContent>
 
-      <CardFooter className="flex cursor-pointer items-center justify-between border-t px-4 py-3 text-sm text-muted-foreground hover:underline">
-        <span>Ver mais detalhes</span>
-        <ArrowRight size={16} />
-      </CardFooter>
+      <Link href={`/occurrence/${id}`}>
+        <CardFooter className="flex items-center justify-between border-t px-4 py-3 text-sm text-muted-foreground hover:underline">
+          <span>Ver mais detalhes</span>
+          <ArrowRight size={16} />
+        </CardFooter>
+      </Link>
     </Card>
   )
 }
