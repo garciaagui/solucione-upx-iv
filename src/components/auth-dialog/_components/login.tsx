e1a34c4a06fc7d0925bde24928ae0667ce61e19c
Guilherme Garcia <garciaguig@gmail.com>
2025-05-12 09:44:20 -0300

    feat: Implement 'login form' component in 'auth' dialog

--- src/components/auth-dialog/_components/login.tsx
+++ src/components/auth-dialog/_components/login.tsx
@@ -1,21 +1,7 @@
 'use client'
 
-import { signIn, SignInResponse } from 'next-auth/react'
-
-import LoadingMessage from '@/components/loading-message'
-import { Button } from '@/components/ui/button'
+import LoginForm from '@/components/login-form'
 import { DialogDescription, DialogHeader, DialogTitle } from '@/components/ui/dialog'
-import {
-  Form,
-  FormControl,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
-} from '@/components/ui/form'
-import { Input } from '@/components/ui/input'
-import { LoginFormValues } from '@/schemas/login'
-import { ToastError, ToastSuccess } from '@/utils/toast'
 import { useAuthDialog } from '../_utils/context'
 
 interface Props {
@@ -23,33 +9,16 @@ interface Props {
 }
 
 export default function Login({ handleOpen }: Props) {
-  const { loading, loginForm, setLoading, setSelectedForm } = useAuthDialog()
-  const { control, handleSubmit, reset } = loginForm
+  const { loginForm, setSelectedForm } = useAuthDialog()
+  const { reset } = loginForm
 
   const handleSelectedFormChange = () => {
     setSelectedForm('register')
     reset()
   }
 
-  const login = async (data: LoginFormValues) => {
-    setLoading(true)
-
-    const response = (await signIn('credentials', {
-      ...data,
-      redirect: false,
-    })) as SignInResponse
-
-    const { ok, error } = response
-
-    if (ok) {
-      ToastSuccess('Login bem-sucedido!')
-      handleOpen(false)
-    } else if (!ok && error) {
-      ToastError(error)
-      console.log(error)
-    }
-
-    setLoading(false)
+  const onSuccess = () => {
+    handleOpen(false)
   }
 
   return (
@@ -59,54 +28,18 @@ export default function Login({ handleOpen }: Props) {
         <DialogDescription>Insira suas credenciais para acessar a plataforma</DialogDescription>
       </DialogHeader>
 
-      <Form {...loginForm}>
-        <form onSubmit={handleSubmit(login)} className="flex flex-col gap-4" id="login-form">
-          <FormField
-            control={control}
-            disabled={loading}
-            name="email"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Email</FormLabel>
-                <FormControl>
-                  <Input placeholder="Seu e-mail" type="email" {...field} />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
+      <LoginForm form={loginForm} onSuccess={onSuccess} />
 
-          <FormField
-            control={control}
-            disabled={loading}
-            name="password"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Senha</FormLabel>
-                <FormControl>
-                  <Input placeholder="Sua senha" type="password" {...field} />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-
-          <Button disabled={loading} type="submit" className="mt-2">
-            {!loading ? 'Entrar' : <LoadingMessage message="Entrando..." />}
-          </Button>
-
-          <div className="mt-4 text-center text-sm">
-            Não tem conta?{' '}
-            <button
-              type="button"
-              className="font-medium text-primary underline transition-all duration-200 ease-in-out hover:text-muted-foreground"
-              onClick={handleSelectedFormChange}
-            >
-              Cadastre-se
-            </button>
-          </div>
-        </form>
-      </Form>
+      <div className="mt-8 text-center text-sm">
+        Não tem conta?{' '}
+        <button
+          type="button"
+          className="font-medium text-primary underline transition-all duration-200 ease-in-out hover:text-muted-foreground"
+          onClick={handleSelectedFormChange}
+        >
+          Cadastre-se
+        </button>
+      </div>
     </>
   )
 }
