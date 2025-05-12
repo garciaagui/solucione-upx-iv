3d72d0507a9cbe11f42be9459444faedacc599a5
Guilherme Garcia <garciaguig@gmail.com>
2025-05-12 09:53:23 -0300

    feat: Implement 'login form' component in 'login' page

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -1,25 +1,14 @@
 'use client'
 
-import { Button } from '@/components/ui/button'
-import {
-  Form,
-  FormControl,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
-} from '@/components/ui/form'
-import { Input } from '@/components/ui/input'
+import LoginForm from '@/components/login-form'
 import { LoginFormValues, loginSchema } from '@/schemas/login'
-import { ToastError, ToastSuccess } from '@/utils/toast'
 import { zodResolver } from '@hookform/resolvers/zod'
-import { signIn, SignInResponse } from 'next-auth/react'
 import { useRouter } from 'next/navigation'
 import { useForm } from 'react-hook-form'
 
 export default function Login() {
   const router = useRouter()
-  const loginForm = useForm<LoginFormValues>({
+  const form = useForm<LoginFormValues>({
     resolver: zodResolver(loginSchema),
     defaultValues: {
       email: '',
@@ -27,63 +16,17 @@ export default function Login() {
     },
   })
 
-  const { handleSubmit, control } = loginForm
-
-  const login = async (data: LoginFormValues) => {
-    const response = (await signIn('credentials', {
-      ...data,
-      redirect: false,
-    })) as SignInResponse
-
-    const { ok, error } = response
-
-    if (ok) {
-      ToastSuccess('Login bem-sucedido!')
-      router.push('/home')
-    } else if (!ok && error) {
-      ToastError(error)
-      console.log(error)
-    }
+  const onSuccess = () => {
+    router.push('/home')
   }
 
   return (
     <main className="flex h-screen flex-col items-center gap-8 px-20 py-48">
       <h3 className="scroll-m-20 text-2xl font-semibold tracking-tight">Seja bem-vindo(a)</h3>
 
-      <Form {...loginForm}>
-        <form onSubmit={handleSubmit(login)} className="flex w-1/2 flex-col gap-4" id="login-form">
-          <FormField
-            control={control}
-            name="email"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Email</FormLabel>
-                <FormControl>
-                  <Input placeholder="Seu e-mail" type="email" {...field} />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-
-          <FormField
-            control={control}
-            name="password"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Senha</FormLabel>
-                <FormControl>
-                  <Input placeholder="Sua senha" type="password" {...field} />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-        </form>
-      </Form>
-      <Button type="submit" className="w-1/2" form="login-form">
-        Entrar
-      </Button>
+      <div className="w-1/2">
+        <LoginForm form={form} onSuccess={onSuccess} />
+      </div>
     </main>
   )
 }
