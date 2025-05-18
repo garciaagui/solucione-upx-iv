fbab3332b26416452f461859c62fe461014d18b4
Guilherme Garcia <garciaguig@gmail.com>
2025-05-18 12:12:22 -0300

    feat: Add icon to 'status badge' component

--- src/components/status-badge.tsx
+++ src/components/status-badge.tsx
@@ -1,4 +1,5 @@
 import { Status } from '@prisma/client'
+import { Check, CircleAlert, Loader } from 'lucide-react'
 import { Badge } from './ui/badge'
 
 interface Props {
@@ -17,5 +18,21 @@ export default function StatusBadge({ status }: Props) {
     }
   }
 
-  return <Badge variant={getVariant()}>{status}</Badge>
+  const getIcon = () => {
+    switch (status) {
+      case 'Aberto':
+        return <CircleAlert size={14} />
+      case 'Andamento':
+        return <Loader size={14} />
+      default:
+        return <Check size={14} />
+    }
+  }
+
+  return (
+    <Badge variant={getVariant()}>
+      {getIcon()}
+      {status}
+    </Badge>
+  )
 }
