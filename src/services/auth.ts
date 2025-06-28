8d100304374497f9ebe58384cbe28733a7bc9b2f
Guilherme Garcia <garciaguig@gmail.com>
2025-06-28 08:28:43 -0300

    feat: Create 'verifyEmail' method on auth service

--- src/services/auth.ts
+++ src/services/auth.ts
@@ -1,5 +1,11 @@
 import api from '@/lib/axios'
-import { LoginRequest, LoginResponse, RegisterRequest, RegisterResponse } from '@/types/auth'
+import {
+  LoginRequest,
+  LoginResponse,
+  RegisterRequest,
+  RegisterResponse,
+  VerifyEmailResponse,
+} from '@/types/auth'
 
 export const login = async (data: LoginRequest): Promise<LoginResponse> => {
   try {
@@ -23,6 +29,16 @@ export const logout = async (): Promise<void> => {
 export const register = async (data: RegisterRequest): Promise<RegisterResponse> => {
   try {
     const response = await api.post('auth/register', data)
+    return response.data
+  } catch (error) {
+    console.log(error)
+    throw error
+  }
+}
+
+export const verifyEmail = async (token: string): Promise<VerifyEmailResponse> => {
+  try {
+    const response = await api.get(`auth/verify-email?token=${token}`)
     return response.data
   } catch (error) {
     console.log(error)
--- src/types/auth.ts
+++ src/types/auth.ts
@@ -22,3 +22,7 @@ export interface RegisterResponse {
     user: UserBasicInfo
   }
 }
+
+export interface VerifyEmailResponse {
+  message: string
+}
